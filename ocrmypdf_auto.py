e3c279b01f9e3a8c4409dbbc99a9a800e19d1b91
Colin McCambridge <colin@mccambridge.org>
2018-06-15 00:12:48 -0500

    Prettier Docker exit

--- ocrmypdf_auto.py
+++ ocrmypdf_auto.py
@@ -158,16 +158,21 @@ class AutoOcrEventHandler(PatternMatchingEventHandler):
 
 class DockerSignalMonitor(object):
 
+    SIGNUMS_TO_NAMES = { getattr(signal, signame) : signame for signame in ['SIGINT', 'SIGTERM'] }
+
     def __init__(self):
         self.exit_event = Event()
+        self.signum = None
         signal.signal(signal.SIGINT, self.handler)
         signal.signal(signal.SIGTERM, self.handler)
 
     def handler(self, signum, frame):
+        self.signum = signum
         self.exit_event.set()
 
     def wait_for_exit(self):
         self.exit_event.wait()
+        return self.signum
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
@@ -191,9 +196,9 @@ if __name__ == "__main__":
         logging.info('Watching %s...', INPUT_BASE)
 
         # Wait in the main thread to be killed
-        docker_monitor.wait_for_exit()
+        signum = docker_monitor.wait_for_exit()
 
-        logging.info('Shutting down...')
+        logging.info('Signal %d (%s) Received. Shutting down...', signum, DockerSignalMonitor.SIGNUMS_TO_NAMES[signum])
 
         tasks = [task for _, task in current_tasks.items()]
         for task in tasks:
