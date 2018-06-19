2b94c76e4bf4260381f887c6abae606169021873
Colin McCambridge <colin@mccambridge.org>
2018-06-18 23:18:02 -0500

    Add support for single-folder and mirror-tree output modes.
    Refactor AutoOcrScheduler and OcrTask to pass input and output path configuration
    along from higher order objects.

--- ocrmypdf_auto.py
+++ ocrmypdf_auto.py
@@ -12,10 +12,6 @@ from docker_support import DockerSignalMonitor
 
 logger = logging.getLogger('ocrmypdf-auto')
 
-INPUT_BASE = local.path(os.getenv('OCR_INPUT_DIR', '/input'))
-OUTPUT_BASE = local.path(os.getenv('OCR_OUTPUT_DIR', '/output'))
-OCRMYPDF = local['ocrmypdf']
-
 class OcrmypdfConfig(object):
 
     def __init__(self, input_path, output_path, config_file=None):
@@ -93,11 +89,14 @@ class OcrTask(object):
     ACTIVE = 'ACTIVE'
     DONE = 'DONE'
 
+    _OCRMYPDF = local['ocrmypdf']
+
     COALESCING_DELAY = timedelta(seconds=try_float(os.getenv('OCR_PROCESSING_DELAY'), 3.0))
 
-    def __init__(self, path, submit_func, done_callback):
+    def __init__(self, input_path, output_path, submit_func, done_callback):
         self.logger = logger.getChild('task')
-        self.path = path
+        self.input_path = input_path
+        self.output_path = output_path
         self.submit_func = submit_func
         self.done_callback = done_callback
         self.last_touch = None
@@ -109,47 +108,48 @@ class OcrTask(object):
         return self.__repr__()
 
     def __repr__(self):
-        return "<OcrTask [{}] {} last_touch={} future={}>".format(
+        return "<OcrTask [{}] {}->{} last_touch={} future={}>".format(
                 self.state,
-                self.path,
+                self.input_path,
+                self.output_path,
                 self.last_touch,
                 self.future)
 
     def enqueue(self):
         assert self.state in [OcrTask.NEW, OcrTask.ACTIVE]
         self.state = OcrTask.QUEUED
-        self.logger.debug('OcrTask Enqueued: [%s] %s', self.state, self.path)
+        self.logger.debug('OcrTask Enqueued: [%s] %s', self.state, self.input_path)
         self.future = self.submit_func(self.process)
 
     def touch(self):
-        self.logger.debug('OcrTask Touched: [%s] %s', self.state, self.path)
+        self.logger.debug('OcrTask Touched: [%s] %s', self.state, self.input_path)
         self.last_touch = datetime.now()
         if self.state == OcrTask.NEW:
             self.enqueue()
 
     def cancel(self):
-        self.logger.debug('OcrTask Canceled: [%s] %s', self.state, self.path)
+        self.logger.debug('OcrTask Canceled: [%s] %s', self.state, self.input_path)
         self.last_touch = None
         if self.state == OcrTask.QUEUED:
             self.future.cancel()
             self.done()
 
     def done(self):
-        self.logger.debug('OcrTask Done: [%s] %s', self.state, self.path)
+        self.logger.debug('OcrTask Done: [%s] %s', self.state, self.input_path)
         self.state = OcrTask.DONE
         if self.done_callback:
             self.done_callback(self)
 
     def process(self, skip_delay=False):
         """ocrmypdf processing task"""
-        self.logger.warn('Processing: %s', self.path)
+        self.logger.warn('Processing: %s -> %s', self.input_path, self.output_path)
         start_ts = datetime.now()
 
         # Coalescing sleep as long as file is still being modified
         while not skip_delay:
             # Check for cancellation
             if self.last_touch is None:
-                self.logger.info('Processing canceled: %s', self.path)
+                self.logger.info('Processing canceled: %s', self.input_path)
                 self.done()
                 return
 
@@ -157,29 +157,30 @@ class OcrTask(object):
             wait_span = due - datetime.now()
             if wait_span <= timedelta(0):
                 break;
-            self.logger.debug('Sleeping for %f: [%s] %s', wait_span.total_seconds(), self.state, self.path)
+            self.logger.debug('Sleeping for %f: [%s] %s', wait_span.total_seconds(), self.state, self.input_path)
             self.state = OcrTask.SLEEPING
             time.sleep(wait_span.total_seconds())
 
         # Actually run ocrmypdf
         self.state = OcrTask.ACTIVE
         self.last_touch = None
-        self.logger.info('Running ocrmypdf: %s', self.path)
+        self.logger.info('Running ocrmypdf: %s', self.input_path)
 
-        # TODO: Take output path from ctor
-        out_path = OUTPUT_BASE / (self.path - INPUT_BASE)
         # TODO: Take config from ctor
-        config = OcrmypdfConfig(self.path, out_path)
-        ocrmypdf = OCRMYPDF.__getitem__(config.get_ocrmypdf_arguments())
+        config = OcrmypdfConfig(self.input_path, self.output_path)
+        if not self.output_path.parent.exists():
+            self.logger.debug('Mkdir: %s', self.output_path.parent)
+            self.output_path.parent.mkdir()
+        ocrmypdf = OcrTask._OCRMYPDF.__getitem__(config.get_ocrmypdf_arguments())
         (rc, stdout, stderr) = ocrmypdf.run()
         self.logger.debug('ocrmypdf returns %d with stdout [%s] and stderr[%s]', rc, stdout, stderr)
 
         runtime = datetime.now() - start_ts
-        self.logger.warn('Processing complete in %f seconds: %s', round(runtime.total_seconds(), 2), self.path)
+        self.logger.warn('Processing complete in %f seconds: %s', round(runtime.total_seconds(), 2), self.input_path)
 
         # Check for modification during sleep
         if self.last_touch is not None:
-            self.logger.info('Modified during ocrmypdf execution: [%s] %s', self.state, self.path)
+            self.logger.info('Modified during ocrmypdf execution: [%s] %s', self.state, self.input_path)
             self.enqueue()
             return
 
@@ -224,13 +225,30 @@ class AutoOcrWatchdogHandler(PatternMatchingEventHandler):
         self.touch_file(local.path(event.src_path))
 
 
+class AutoOcrSchedulerException(Exception):
+    pass
+
 class AutoOcrScheduler(object):
 
-    def __init__(self):
+    SINGLE_FOLDER = 'single_folder'
+    MIRROR_TREE = 'mirror_tree'
+
+    OUTPUT_MODES = [SINGLE_FOLDER, MIRROR_TREE]
+
+    def __init__(self, input_dir, output_dir, output_mode):
         self.logger = logger.getChild('scheduler')
 
-        self.input_path = INPUT_BASE
+        self.input_dir = local.path(input_dir)
+        self.output_dir = local.path(output_dir)
+        if self.input_dir == self.output_dir:
+            raise AutoOcrSchedulerException('Invalid configuration. Input and output directories must not be the same to avoid recursive OCR invocation!')
+
+        self.output_mode = output_mode.lower()
+        if self.output_mode not in AutoOcrScheduler.OUTPUT_MODES:
+            raise AutoOcrSchedulerException('Invalid output mode: {}. Must be one of: {}'.format(self.output_mode, ','.join(AutoOcrScheduler.OUTPUT_MODES)))
+
         self.current_tasks = {}
+        self.current_outputs = set()
 
         # Create a Threadpool to run OCR tasks on
         self.threadpool = ThreadPoolExecutor(max_workers=3)
@@ -238,11 +256,11 @@ class AutoOcrScheduler(object):
         # Wire up an AutoOcrWatchdogHandler
         watchdog_handler = AutoOcrWatchdogHandler(self.on_file_touched, self.on_file_deleted)
 
-        # Schedule watchdog to observe the input path
+        # Schedule watchdog to observe the input directory
         self.observer = Observer()
-        self.observer.schedule(watchdog_handler, INPUT_BASE, recursive=True)
+        self.observer.schedule(watchdog_handler, self.input_dir, recursive=True)
         self.observer.start()
-        self.logger.warn('Watching %s', self.input_path)
+        self.logger.warn('Watching %s', self.input_dir)
 
     def shutdown(self):
         # Shut down the feed of incoming watchdog events
@@ -276,18 +294,42 @@ class AutoOcrScheduler(object):
         self.shutdown()
         return False
 
+    def _map_output_path(self, input_path):
+        if self.output_mode == AutoOcrScheduler.MIRROR_TREE:
+            return self.output_dir / (input_path - self.input_dir)
+        else:
+            assert self.output_mode == AutoOcrScheduler.SINGLE_FOLDER
+            output_path = self.output_dir / (input_path.name)
+            unique = 1
+            if output_path.exists() or output_path in self.current_outputs:
+                suffix = '.{}.{}{}'.format(datetime.now().strftime('%Y%m%d'), unique, output_path.suffix)
+                output_path = output_path.with_suffix(suffix)
+
+            while output_path.exists() or output_path in self.current_outputs:
+                unique = unique + 1
+                output_path = output_path.with_suffix('.{}{}'.format(unique, output_path.suffix), depth=2)
+            return output_path
+
+
+    def queue_path(self, path):
+        output_path = self._map_output_path(path)
+        task = OcrTask(path, output_path, self.threadpool.submit, self.on_task_done)
+        self.current_tasks[path] = task
+        self.current_outputs.add(output_path)
+
     def on_file_touched(self, path):
         if path in self.current_tasks:
             self.current_tasks[path].touch()
         else:
-            self.current_tasks[path] = OcrTask(path, self.threadpool.submit, self.on_task_done)
+            self.queue_path(path)
 
     def on_file_deleted(self, path):
         if path in self.current_tasks:
             self.current_tasks[path].cancel()
 
     def on_task_done(self, task):
-        del self.current_tasks[task.path]
+        self.current_outputs.remove(task.output_path)
+        del self.current_tasks[task.input_path]
 
 
 if __name__ == "__main__":
@@ -310,8 +352,12 @@ if __name__ == "__main__":
 
     docker_monitor = DockerSignalMonitor()
 
+    input_dir = local.path(os.getenv('OCR_INPUT_DIR', '/input'))
+    output_dir = local.path(os.getenv('OCR_OUTPUT_DIR', '/output'))
+    output_mode = os.getenv('OCR_OUTPUT_MODE', AutoOcrScheduler.MIRROR_TREE)
+
     # Run an AutoOcrScheduler until terminated
-    with AutoOcrScheduler() as scheduler:
+    with AutoOcrScheduler(input_dir, output_dir, output_mode) as scheduler:
         # Wait in the main thread to be killed
         signum = docker_monitor.wait_for_exit()
         logger.warn('Signal %d (%s) Received. Shutting down...', signum, DockerSignalMonitor.SIGNUMS_TO_NAMES[signum])
